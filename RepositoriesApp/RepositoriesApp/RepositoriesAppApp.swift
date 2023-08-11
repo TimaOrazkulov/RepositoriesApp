facc9277d2ed160e104f5514f8fcfe31a588b60d
Temirlan Orazkulov <tima.orazkulov@gmail.com>
2023-08-11 14:42:36 +0600

    [feat] initial commit

---
+++ RepositoriesApp/RepositoriesApp/RepositoriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepositoriesAppApp.swift
+//  RepositoriesApp
+//
+//  Created by Temirlan Orazkulov on 11.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RepositoriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
